344e366f71c604e9b39d20ad22354712092a629a
gavinarori <arorigavin@gmail.com>
2024-10-01 14:11:12 +0300

    refining breakpoints

--- src/components/PreviewTitle.jsx
+++ src/components/PreviewTitle.jsx
@@ -1,14 +1,8 @@
 export default function PreviewTitle({ componentTitle, componentHash }) {
   return (
-    <h2 className="text-xl font-bold text-gray-900 sm:text-2xl">
+    <h2 className="text-lg font-bold text-gray-900 sm:text-2xl">
       <a href={`#${componentHash}`} className="group relative inline-block">
-        <span
-          aria-hidden="true"
-          className="hidden group-hover:opacity-25 group-focus:opacity-25 lg:absolute lg:inset-y-0 lg:-left-6 lg:block lg:opacity-0 lg:transition"
-        >
-          #
-        </span>
-
+        
         {componentTitle}
       </a>
     </h2>
