54a188efb09dc9d66fd37f71d5c846d8778d762a
gavinarori <arorigavin@gmail.com>
2024-10-01 12:59:36 +0300

    added select and copy-button

--- src/components/PreviewCopy.jsx
+++ src/components/PreviewCopy.jsx
@@ -1,50 +1,46 @@
-import { useState } from 'react'
+import { useState } from "react";
+import { useCopyToClipboard } from "react-use";
+import { CheckIcon, ClipboardIcon } from "lucide-react";
 
-import { useCopyToClipboard } from 'react-use'
-
-import ButtonStyle from '@component/ButtonStyle'
-
-export default function PreviewCopy({ codeType, componentCode = '' }) {
-  const [buttonText, setButtonText] = useState('Copy')
-  const [buttonEmoji, setButtonEmoji] = useState('📋')
-  const [copyStatus, copyToClipboard] = useCopyToClipboard()
-
-  const buttonActive = buttonText === 'Copied'
+export default function PreviewCopy({ codeType, componentCode = "" }) {
+  const [buttonText, setButtonText] = useState("Copy");
+  const [copyStatus, copyToClipboard] = useCopyToClipboard();
+  
+  const buttonActive = buttonText === "Copied";
 
   const codeTypeMap = {
-    html: 'HTML',
-    jsx: 'JSX',
-    vue: 'Vue',
-  }
+    html: "HTML",
+    jsx: "JSX",
+    vue: "Vue",
+  };
 
-  const codeTypeLabel = codeTypeMap[codeType]
+  const codeTypeLabel = codeTypeMap[codeType];
 
   function handleCopyToClipboard() {
-    copyToClipboard(componentCode)
+    copyToClipboard(componentCode);
 
     if (copyStatus.error) {
-      setButtonText('Error')
-      setButtonEmoji('🚨')
-
-      return
+      setButtonText("Error");
+      return;
     }
 
-    setButtonText('Copied')
-    setButtonEmoji('🎉')
+    setButtonText("Copied");
 
     setTimeout(() => {
-      setButtonText('Copy')
-      setButtonEmoji('📋')
-    }, 3000)
+      setButtonText("Copy");
+    }, 3000);
   }
 
+  const hasCopied = buttonText === "Copied"; // Check if it's copied
+
   return (
-    <button className="hidden sm:block" onClick={handleCopyToClipboard}>
-      <ButtonStyle
-        buttonEmoji={buttonEmoji}
-        buttonText={`${buttonText} ${codeTypeLabel}`}
-        buttonActive={buttonActive}
-      />
+    <button
+      className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
+      onClick={handleCopyToClipboard}
+    >
+      <span className="sr-only">Copy {codeTypeLabel}</span>
+      {hasCopied ? <CheckIcon className="mr-2 h-4 w-4" /> : <ClipboardIcon className="mr-2 h-4 w-4" />}
+      {buttonText}
     </button>
-  )
+  );
 }
