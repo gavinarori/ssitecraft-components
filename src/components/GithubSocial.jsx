1efd1ab272c6c0066d9ac7fe78d6abd6105a41a1
gavinarori <arorigavin@gmail.com>
2024-12-10 16:50:01 +0300

    updated the site Navbar

--- src/components/GithubSocial.jsx
+++ src/components/GithubSocial.jsx
@@ -3,12 +3,12 @@ import IconGithub from '@component/IconGithub'
 export default function GithubSocial() {
   return (
     <a
-      href="https://github.com/markmead/hyperui"
+      href="https://www.buymeacoffee.com/arorigavin"
       rel="noreferrer"
       target="_blank"
       className="text-gray-900 hover:opacity-75"
     >
-      <span className="sr-only"> GitHub </span>
+      <span className="sr-only"> Buy me coffee </span>
 
       <IconGithub />
     </a>
--- src/components/HeaderMenu.jsx
+++ src/components/HeaderMenu.jsx
@@ -1,7 +1,6 @@
-import IconMenu from '@component/IconMenu'
 import MenuLinks from '@component/HeaderMenuLinks'
 
-export default function HeaderMenu({ showMenu, handleSetShowMenu}) {
+export default function HeaderMenu({ showMenu, handleSetShowMenu, menuLinks }) {
   return (
     <div className="flex items-center md:hidden">
       <button onClick={() => handleSetShowMenu(!showMenu)} className="text-gray-900">
@@ -20,5 +19,20 @@ export default function HeaderMenu({ showMenu, handleSetShowMenu}) {
         </div>
       )}
     </div>
+  )
+}
+
+function IconMenu() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="size-5"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      strokeWidth="2"
+    >
+      <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
+    </svg>
   )
 }
